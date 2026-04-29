adb6f4120f00d959932909728aeb4c19939cd9db
MadCheshiren <fraginalkirrenmichael@gmail.com>
2026-04-29 11:48:27 +0800

    try

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service Worker - Caches assets for offline access */
 
 // Cache Configuration
-const CACHE_NAME = 'portfolio-v4';
+const CACHE_NAME = 'portfolio-v5';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
